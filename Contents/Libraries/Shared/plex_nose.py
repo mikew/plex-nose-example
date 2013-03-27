8c14c4a6070b78d0b1daa76369cdaadae627c250
Mike Wyatt <wyatt.mike@gmail.com>
2013-03-27 18:44:27 -0230

    adds plex_nose.publish_local_file

--- Contents/Libraries/Shared/plex_nose.py
+++ Contents/Libraries/Shared/plex_nose.py
@@ -1,5 +1,16 @@
 core = None
 
+def publish_local_file(local_path, name = None):
+    import os
+
+    local_path = os.path.abspath(__file__ + '/../../../../' + local_path)
+    local_file = open(local_path, 'r')
+    contents   = local_file.read()
+    local_file.close()
+
+    if not name: name = os.path.basename(local_path)
+    core.sandbox.publish_api(contents, name = name)
+
 def sandbox(f):
     import nose
 
