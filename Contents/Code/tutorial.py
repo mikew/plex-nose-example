ebd1b46d7dac0ca2522772afcc7e197ce5ff732a
Mike Wyatt <wyatt.mike@gmail.com>
2013-04-09 01:52:27 -0230

    move what we can to consts.py

--- Contents/Code/tutorial.py
+++ Contents/Code/tutorial.py
@@ -1,8 +1,6 @@
-import consts
-
+consts = SharedCodeService.consts
 main_sections = [ 'all', 'interviews', 'reviews', 'gameplay', 'shows',
 'trailers', 'previews' ]
-print SharedCodeService.test
 
 def MainMenu():
     container = ObjectContainer(title1 = L('title'))
@@ -19,31 +17,21 @@ def MainMenu():
 @route('%s/{section}' % consts.prefix)
 def MosiacMenu(section, page = 1):
     container = ObjectContainer(title1 = L('title.%s' % section))
-    url       = url_for_section(section) + '?page=%s' % page
+    url       = consts.url_for_section(section) + '?page=%s' % page
     root      = HTML.ElementFromURL(url)
     videos    = root.cssselect('#video_stream a')
-    latest    = root.cssselect('h1.video_title')
+    latest    = consts.video_from(root)
 
     if latest:
-        url   = root.cssselect('#flash_req_msg video')[0].get('src')
-        title = latest[0].text_content().strip()
-        video = VideoClipObject(title = title, url = url)
+        video = VideoClipObject(title = latest['title'], url = latest['url'])
 
         container.add(video)
 
     for video in videos:
-        url   = 'http://www.gamespot.com' + video.get('href')
+        url   = consts.permalink_for(video.get('href'))
         title = video.text_content().strip()
         video = VideoClipObject(title = title, url = url)
 
         container.add(video)
 
     return container
-
-def url_for_section(section):
-    base_url = 'http://www.gamespot.com/videos/'
-
-    if 'all' == section:
-        return base_url
-    else:
-        return base_url + section + '/'
